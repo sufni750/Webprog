c76e17d5e0109ab82a5d9aec7f3adbb6ec556382
sufni750 <sufni750@gmail.com>
2023-04-22 19:49:45 +0200

    kalkulátor űrlap hozzáadva

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -1,2 +1,4 @@
 <h2>Üdv!</h2>
-
+<p>Egy egyszerű hobbi oldal a 3D nyomtatásról</p>
+<p>Vigyázat! Függőséget okoz :) </p>
+<img src="./images/cimlapra.jpg" alt=" " height="200px" style="vertical-align:middle">
--- templates/pages/kalkulator.tpl.php
+++ templates/pages/kalkulator.tpl.php
@@ -1,1 +1,24 @@
 <h2>Kalkulátor</h2>
+
+<form action="/kiszamol.php"> 
+<label for="hossz">Hossz (mm): </label><br>
+<input type="number" id="hosszmezo" name="hossz" value="John"><br><br>
+
+<label for="atmero">Átmérő:</label>
+<select name="atmero" id="atmero">
+  <option value="1,75">1,75 mm2</option>
+  <option value="3">3 mm2</option>
+</select> 
+
+<label for="anyagok">Anyag:</label>
+<select name="anyagok" id="anyagok">
+  <option value="abs">ABS</option>
+  <option value="pla">PLA</option>
+  <option value="petg">PETG</option>
+</select> 
+
+
+
+ <br><br>
+  <input type="submit" value="Kiszámol">
+</form>
